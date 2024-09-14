8af5c6b20120fa9097555b1f4ce204574a856dea
pesala-x <gkpesalawinodith@gmail.com>
2024-09-14 12:09:28 +0530

    Tested Quotes to Scrape with csv

--- main/Quotes to Scrape.py
+++ main/Quotes to Scrape.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 pageToScrape = requests.get("https://quotes.toscrape.com/")
 soup = BeautifulSoup(pageToScrape.text, "html.parser")
@@ -7,12 +8,24 @@ soup = BeautifulSoup(pageToScrape.text, "html.parser")
 quotes = soup.find_all("span", attrs={"class": "text"})
 authors = soup.find_all("small", attrs={"class": "author"})
 
+file = open("scraped_quotes.csv", "w")
+writer = csv.writer(file)
+
+writer.writerow(["QUOTES", "AUTHORS"])
+
 """
-# this is simple code 
+# this is simple code for get authors and quotes
 for quote in quotes:
     print(quote.text)
 for author in authors:
     print(author.text)
 """
+"""
 for quote, author in zip(quotes, authors):
     print(quote.text + " --> " + author.text)
+"""
+
+for quote, author in zip(quotes, authors):
+    print(quote.text + " --> " + author.text)
+    writer.writerow([quote.text, author.text])
+    # file.close()
